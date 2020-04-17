fcf9afdfe55234980f642fc70938e58639f4920c
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-04-17 13:48:31 +0530

    feat: updated to angular 9

--- projects/ngx-bulma/src/lib/modal/index.ts
+++ projects/ngx-bulma/src/lib/modal/index.ts
@@ -4,3 +4,4 @@ export * from './modal.module';
 export * from './model-head/model-head.component';
 export * from './model-content/model-content.component';
 export * from './model-footer/model-footer.component';
+export * from './model-title/model-title.component';
