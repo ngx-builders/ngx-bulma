602454369317eb56025b772b4c1884c57b7a1e09
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2020-04-30 23:18:13 +0530

    docs: fix for issue #292 and # 294

--- projects/bulma-app/src/app/navbar/navbar.component.ts
+++ projects/bulma-app/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  @ViewChild('navBurger') navBurger: ElementRef;
+  @ViewChild('navMenu') navMenu: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleNavbar() {
+    this.navBurger.nativeElement.classList.toggle('is-active');
+    this.navMenu.nativeElement.classList.toggle('is-active');
   }
 
 }
--- projects/ngx-bulma/src/lib/navbar/navbar.module.ts
+++ projects/ngx-bulma/src/lib/navbar/navbar.module.ts
@@ -5,6 +5,8 @@ import { BulmaNavbarBrandComponent } from './navbar-brand/navbar-brand.component
 import { NavbarDropdownComponent } from './navbar-dropdown/navbar-dropdown.component';
 import { NavbarLinkComponent } from './navbar-link/navbar-link.component';
 import { BulmaNavbarComponent } from './navbar.component';
+import { NavbarStartComponent } from './navbar-start/navbar-start.component';
+import { NavbarEndComponent } from './navbar-end/navbar-end.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { BulmaNavbarComponent } from './navbar.component';
     BulmaNavbarBrandComponent,
     NavbarLinkComponent,
     NavbarDropdownComponent,
-    NavItemDirective
+    NavItemDirective,
+    NavbarStartComponent,
+    NavbarEndComponent
   ],
   imports: [
     CommonModule
@@ -22,7 +26,9 @@ import { BulmaNavbarComponent } from './navbar.component';
     BulmaNavbarBrandComponent,
     NavbarLinkComponent,
     NavbarDropdownComponent,
-    NavItemDirective
+    NavItemDirective,
+    NavbarStartComponent,
+    NavbarEndComponent
   ]
 })
 export class BulmaNavbarModule { }
