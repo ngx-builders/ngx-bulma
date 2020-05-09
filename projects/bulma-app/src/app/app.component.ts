4acf58257320558e938bad2c8d78eb8c68192bbb
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-05-09 17:09:27 +0530

    feat: seperate outlet fot introduction

--- projects/bulma-app/src/app/app.component.ts
+++ projects/bulma-app/src/app/app.component.ts
@@ -1,16 +1,34 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   @ViewChild('navBurger') navBurger: ElementRef;
   @ViewChild('navMenu') navMenu: ElementRef;
 
-  title = 'bulma-app';
+  currentRoute = '';
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(e => e instanceof RouterEvent)
+    ).subscribe(e => {
+      if (e instanceof NavigationEnd) {
+        this.currentRoute = e.url;
+      }
+    });
+
+    // console.log(this.currentRoute);
+  }
 
   toggleNavbar() {
     this.navBurger.nativeElement.classList.toggle('is-active');
