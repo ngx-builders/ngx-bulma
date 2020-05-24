36686f0b541f7d41ef170873d42070911c35b006
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2020-05-24 22:04:32 +0530

    docs: configure prettier

--- projects/bulma-app/src/app/button/button.module.ts
+++ projects/bulma-app/src/app/button/button.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import {BulmaButtonModule} from 'ngx-bulma';
+import { BulmaButtonModule } from 'ngx-bulma';
 import { MarkdownModule } from 'ngx-markdown';
-import {ButtonRoutingModule} from './button-routing.module';
-import {ButtonComponent} from './button.component';
+import { ButtonRoutingModule } from './button-routing.module';
+import { ButtonComponent } from './button.component';
 
 @NgModule({
   declarations: [ButtonComponent],
@@ -12,8 +12,8 @@ import {ButtonComponent} from './button.component';
     CommonModule,
     BulmaButtonModule,
     ButtonRoutingModule,
-    MarkdownModule.forRoot({ loader: HttpClient })
+    MarkdownModule.forRoot({ loader: HttpClient }),
   ],
-  exports: [ButtonComponent]
+  exports: [ButtonComponent],
 })
-export class ButtonModule { }
+export class ButtonModule {}
