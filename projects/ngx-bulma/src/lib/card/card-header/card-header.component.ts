6c38d6d93f1caa1cdeab5992d2708c16b02040b4
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-06-14 11:40:06 +0530

    fix: card component padding

--- projects/ngx-bulma/src/lib/card/card-header/card-header.component.ts
+++ projects/ngx-bulma/src/lib/card/card-header/card-header.component.ts
@@ -1,17 +1,22 @@
-import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'bu-card-header',
   templateUrl: './card-header.component.html',
   styleUrls: ['./card-header.component.css'],
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BulmaCardHeaderComponent implements OnInit {
+  @HostBinding('class') class = 'card-header';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
