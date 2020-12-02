bdc47466a481ac6981dc73377c1fc1af34bce0c9
Surya Teja K <shanmukhateja@gmail.com>
2020-12-02 17:24:18 +0530

    style(table): Add curly braces for table.directive if loop

--- projects/ngx-bulma/table/table.directive.ts
+++ projects/ngx-bulma/table/table.directive.ts
@@ -1,45 +1,45 @@
 import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[buTable]'
+  selector: '[buTable]',
 })
 export class BulmaTableDirective implements OnInit {
+  @Input()
+  bordered = false;
 
   @Input()
-  bordered = false
+  striped = false;
 
   @Input()
-  striped = false
+  narrower = false;
 
   @Input()
-  narrower = false
+  hoverable = false;
 
   @Input()
-  hoverable = false
+  fullWidth = false;
 
-  @Input()
-  fullWidth = false
-
-  constructor(
-    private el: ElementRef,
-    private renderer: Renderer2
-  ) { }
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    if (this.bordered)
-      this.renderer.addClass(this.el.nativeElement, 'is-bordered')
+    if (this.bordered) {
+      this.renderer.addClass(this.el.nativeElement, 'is-bordered');
+    }
 
-    if (this.striped)
-      this.renderer.addClass(this.el.nativeElement, 'is-striped')
+    if (this.striped) {
+      this.renderer.addClass(this.el.nativeElement, 'is-striped');
+    }
 
-    if (this.narrower)
-      this.renderer.addClass(this.el.nativeElement, 'is-narrow')
+    if (this.narrower) {
+      this.renderer.addClass(this.el.nativeElement, 'is-narrow');
+    }
 
-    if (this.hoverable)
-      this.renderer.addClass(this.el.nativeElement, 'is-hoverable')
+    if (this.hoverable) {
+      this.renderer.addClass(this.el.nativeElement, 'is-hoverable');
+    }
 
-    if (this.fullWidth)
-      this.renderer.addClass(this.el.nativeElement, 'is-fullwidth')
-
+    if (this.fullWidth) {
+      this.renderer.addClass(this.el.nativeElement, 'is-fullwidth');
+    }
   }
 }
